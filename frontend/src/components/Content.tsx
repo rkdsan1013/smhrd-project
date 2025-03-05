d61c6cabe93ac9afffbec924d3adb0dc444e7f4a
rkdsan1013 <rkdsan1013@gmail.com>
2025-03-05 12:25:47 +0900

    commit test

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Content: React.FC = () => {
   return (
     <main className="content">
-      <h2>메인 컨텐츠</h2>
+      <h2>집가고싶다</h2>
       {/* 메인 컨텐츠 내용 추가 */}
     </main>
   );
